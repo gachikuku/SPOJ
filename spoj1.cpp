3e3b8f4770b7ee3b0940a9177f86d44ee4f403bf
Alnoman Kamil <alneihzen@gmail.com>
2021-11-10 13:11:46 +0200

    Add files via upload

---
+++ spoj1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve(){
+
+    while (1){
+    
+        int n;
+        
+        cin >> n;
+        
+        if (n==42)
+            break;
+        
+        cout<<n<<"\n";
+    }
+}
+
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    
+    cin.tie(0);
+    
+    solve();
+    
+    return 0;
+}
